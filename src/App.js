262e09bbd5a327f90fd74ef4ff8530e264a161ad
taegyuni <taegyun7553@naver.com>
2022-10-19 21:39:04 +0900

    Feat: scr방식으로 빠른 화면전환기능

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
+import RouteTest from './components/RouteTest';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary" element={<Diary />} />
         </Routes>
+        <RouteTest />
       </div>
     </BrowserRouter>
   );
