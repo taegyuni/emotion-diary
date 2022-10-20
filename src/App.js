dd4c47f24d446ceaa579682c362914220acf1256
taegyuni <taegyun7553@naver.com>
2022-10-20 17:39:01 +0900

    Feat: react router 기능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
-import New from './pages/New';
-import Edit from './pages/Edit';
-import Diary from './pages/Diary';
-import RouteTest from './components/RouteTest';
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import RouteTest from "./components/RouteTest";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
         <RouteTest />
       </div>
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,7 @@
+import { useParams } from "react-router-dom";
 const Diary = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div>
       <h1>Diary</h1>
