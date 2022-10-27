6fbcd2f3710e69e3601ceb9f6bc5e02a4f77552f
taegyuni <taegyun7553@naver.com>
2022-10-27 23:24:11 +0900

    Feat: 새 일기 쓰기 날짜 선택 추가

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 일기 추가</p>
+      <MyHeader
+        headtext={"새 일기쓰기"}
+        leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
