88ef52ab60b2f990911277391b831a8b204fbab6
taegyuni <taegyun7553@naver.com>
2022-11-03 21:02:25 +0900

    Feat: 일기 수정 페이지 추가

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,39 +1,33 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const id = searchParams.get("id");
-  console.log("id:", id);
+  const { id } = useParams();
 
-  const mode = searchParams.get("mode");
-  console.log("mode:", mode);
+  const diaryList = useContext(DiaryStateContext);
+  console.log(id);
+  console.log(diaryList);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정</p>
-
-      {/* 쿼리 변경 가능. 그래서 useSearchParams를 사용 */}
-      <button onClick={() => setSearchParams({ who: "taegyun" })}>
-        QS 바꾸기
-      </button>
-
-      {/* 링크 태그 없이 네비게이트 가능 */}
-      <button
-        onClick={() => {
-          navigate("/home");
-        }}
-      >
-        HOME으로 가기
-      </button>
-
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
